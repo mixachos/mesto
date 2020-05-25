51e1256b3a41e022ddee72d4df862d67cde6ab88
Mikhail Shatov <qcd-kirov@yandex.ru>
2020-05-25 14:06:28 +0300

    2nd review

--- scripts/script.js
+++ scripts/script.js
@@ -3,35 +3,41 @@ const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Фото Архыз'
   },
   {
     name: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Фото Челябинская область'
   },
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Фото Иваново'
   },
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Фото Камчатка'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Фото Холмогорский район'
   },
   {
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Фото Байкал'
   }
 ];
 //--
 
 //--Находим блоки
 const profileElement = document.querySelector('.profile');
-const popupElEdit = document.querySelector('.popup_type_edit');
-const popupElAdd = document.querySelector('.popup_type_add');
-const popupElView = document.querySelector('.popup_type_view');
+const popupElementEdit = document.querySelector('.popup_type_edit');
+const popupElementAdd = document.querySelector('.popup_type_add');
+const popupElementView = document.querySelector('.popup_type_view');
 //--
 
 //--Элементы на странице
@@ -42,22 +48,22 @@ const jobElement = profileElement.querySelector('.profile__job');
 //--
 
 //--Элементы формы edit
-const closeButtonEdit = popupElEdit.querySelector('.popup__close-button');
-const saveButtonEdit = popupElEdit.querySelector('.popup__save-button');
-const nameInput = popupElEdit.querySelector('.popup__input_type_name');
-const jobInput = popupElEdit.querySelector('.popup__input_type_job');
+const closeButtonEdit = popupElementEdit.querySelector('.popup__close-button');
+const saveButtonEdit = popupElementEdit.querySelector('.popup__save-button');
+const nameInput = popupElementEdit.querySelector('.popup__input_type_name');
+const jobInput = popupElementEdit.querySelector('.popup__input_type_job');
 //--
 
 //--Элементы формы add
-const closeButtonAdd = popupElAdd.querySelector('.popup__close-button');
-const saveButtonAdd = popupElAdd.querySelector('.popup__save-button');
-const titleInput = popupElAdd.querySelector('.popup__input_type_title');
-const linkInput = popupElAdd.querySelector('.popup__input_type_link');
+const closeButtonAdd = popupElementAdd.querySelector('.popup__close-button');
+const saveButtonAdd = popupElementAdd.querySelector('.popup__save-button');
+const titleInput = popupElementAdd.querySelector('.popup__input_type_title');
+const linkInput = popupElementAdd.querySelector('.popup__input_type_link');
 //--
 
 //--Элементы формы view
-const closeButtonView = popupElView.querySelector('.popup__close-button');
-const captionElement = popupElView.querySelector('.popup__image-caption');
+const closeButtonView = popupElementView.querySelector('.popup__close-button');
+const captionElement = popupElementView.querySelector('.popup__image-caption');
 //--
 
 //--Шаблон карточки
@@ -70,7 +76,7 @@ function initPage() {
   initialCards.forEach(item => {
     const cardItem = cardTemplate.cloneNode(true);
 
-    addCardContent(item.link, item.name, cardItem);
+    addCardContent(item.link, item.name, item.alt, cardItem);
     cardsList.append(cardItem);  //добавили карточку в конец контейнера (списка)
   });
 }
@@ -85,8 +91,8 @@ function initPopupEdit() {
 
 //--Инициализация формы view просмотра картинки
 function initPopupView(evt) {
-  let imageElement = popupElView.querySelector('.popup__image');
-  let captionElement = popupElView.querySelector('.popup__image-caption');
+  const imageElement = popupElementView.querySelector('.popup__image');
+  const captionElement = popupElementView.querySelector('.popup__image-caption');
 
   imageElement.src = evt.target.getAttribute('src');
   captionElement.textContent = evt.target.parentElement.querySelector('.cards__caption-text').textContent;
@@ -104,41 +110,56 @@ function formSubmitHandler(evt) {
 }
 //--
 
+//--Обработка нажатия esc, закрытие попапа
+function escPressed(popup) {
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      popup.classList.remove('popup_opened');
+    }
+  });
+}
+//--
+
 //--Управляем видимостью формы, при открытии - инициализация
 function popupOpenClose(evt) {
 
-  let popupType = evt.target.parentElement.parentElement; //определяем попап, в котором нажата кнопка
-  let clickedButton = evt.target  //определяем, какая кнопка нажата
+  const popupType = evt.target.parentElement.parentElement; //определяем попап, в котором нажата кнопка
+  const clickedButton = evt.target  //определяем, какая кнопка нажата
 
   //определяем, на форме какого попапа произошёл submit
-  let isPopupEditSave = clickedButton.parentElement.classList.contains('popup_type_edit');
-  let isPopupAddSave = clickedButton.parentElement.classList.contains('popup_type_add');
+  const isPopupEditSave = clickedButton.parentElement.classList.contains('popup_type_edit');
+  const isPopupAddSave = clickedButton.parentElement.classList.contains('popup_type_add');
 
   //определяем попап для закрытия (второе условие в if)
-  let isPopupEdit = popupType.classList.contains('popup_type_edit');
-  let isPopupAdd = popupType.classList.contains('popup_type_add');
-  let isPopupView = popupType.classList.contains('popup_type_view');
+  const isPopupEdit = popupType.classList.contains('popup_type_edit');
+  const isPopupAdd = popupType.classList.contains('popup_type_add');
+  const isPopupView = popupType.classList.contains('popup_type_view');
 
   //определяем открытость попапа (для инициализации). add не смотрим, т.к. в него не считываются данные со страницы
-  let isPopupEditOpened = popupElEdit.classList.contains('popup_opened');
-  let isPopupViewOpened = popupElView.classList.contains('popup_opened');
+  const isPopupEditOpened = popupElementEdit.classList.contains('popup_opened');
+  const isPopupViewOpened = popupElementView.classList.contains('popup_opened');
 
   //определяем состояние конкретной кнопки
-  let isEditButton = clickedButton.classList.contains('profile__edit-button');
-  let isAddButton = clickedButton.classList.contains('profile__add-button');
-  let isImageButton = clickedButton.classList.contains('cards__image');
+  const isEditButton = clickedButton.classList.contains('profile__edit-button');
+  const isAddButton = clickedButton.classList.contains('profile__add-button');
+  const isImageButton = clickedButton.classList.contains('cards__image');
 
   //инициализация, открытие и закрытие формы edit
   if (isEditButton || isPopupEdit || isPopupEditSave) {
     if (!isPopupEditOpened) {
       initPopupEdit();
     }
-    popupElEdit.classList.toggle('popup_opened');
+    popupElementEdit.classList.toggle('popup_opened');
+
+    popupElementEdit.parentElement.addEventListener('keydown', function () {escPressed(popupElementEdit)});
+
   }
 
   //открытие и закрытие формы add
   if (isAddButton || isPopupAdd || isPopupAddSave) {
-    popupElAdd.classList.toggle('popup_opened');
+    popupElementAdd.classList.toggle('popup_opened');
+
+    popupElementAdd.parentElement.addEventListener('keydown', function () {escPressed(popupElementAdd)});
 
     titleInput.value = '';
     linkInput.value = '';
@@ -150,7 +171,9 @@ function popupOpenClose(evt) {
     if (!isPopupViewOpened) {
       initPopupView(evt); //evt передаём, чтобы знать, на какой картинке щёлкнули
     }
-    popupElView.classList.toggle('popup_opened');
+    popupElementView.classList.toggle('popup_opened');
+
+    popupElementView.parentElement.addEventListener('keydown', function () {escPressed(popupElementView)});
   }
 }
 //--
@@ -161,17 +184,18 @@ function addCard(evt) {
 
   const cardItem = cardTemplate.cloneNode(true); //клонируем содержимое шаблона
 
-  addCardContent(linkInput.value, titleInput.value, cardItem); //передаём аргументами данные из формы и разметку из шаблона
+  addCardContent(linkInput.value, titleInput.value, `Фото ${titleInput.value}`, cardItem); //передаём аргументами данные из формы и разметку из шаблона
   cardsList.prepend(cardItem);  //добавляем новую карточку в начало списка
   popupOpenClose(evt);
 }
 //--
 
 //--Общая add, для формы добавления и инициализации страницы
-function addCardContent(source, caption, card) {
+function addCardContent(source, caption, alt, card) {
 
   card.querySelector('.cards__image').src = source; //картинке карточки присваиваем в атрибут src значение, переданное через source
   card.querySelector('.cards__caption-text').textContent = caption;   //заполняем содержимое надписи
+  card.querySelector('.cards__image').alt = alt;
 
   //Навешиваем слушатели кнопкам лайк, удалить и открыть просмотр при щелчке по картинке
   card.querySelector('.cards__like-button').addEventListener('click', function (evt) {
@@ -196,9 +220,6 @@ closeButtonEdit.addEventListener('click', popupOpenClose);
 closeButtonAdd.addEventListener('click', popupOpenClose);
 closeButtonView.addEventListener('click', popupOpenClose);
 
-//saveButtonEdit.addEventListener('click', formSubmitHandler);
-//saveButtonAdd.addEventListener('click', addCard);
-
-popupElAdd.addEventListener('submit', addCard); //обработка submit с формы предотвращает отправку пустой формы, поэтому клики на кнопках меняем на submit формы
-popupElEdit.addEventListener('submit', formSubmitHandler);
+popupElementAdd.addEventListener('submit', addCard); //обработка submit с формы предотвращает отправку пустой формы, поэтому клики на кнопках меняем на submit формы
+popupElementEdit.addEventListener('submit', formSubmitHandler);
 //--
